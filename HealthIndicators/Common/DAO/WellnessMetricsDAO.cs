1d92885babaa7599f827dd474e2547f16262e901
randrianalisoa.lalabritta <lala-britta-lau.randrianalisoa@etu.univ-littoral.fr>
2025-01-22 14:20:55 +0100

    test

---
+++ HealthIndicators/Common/DAO/WellnessMetricsDAO.cs
@@ -0,0 +1,12 @@
+using Common.DAO;
+
+namespace Common.DTO;
+
+public class WellnessMetricsDAO
+{
+    public int Id { get; set; }
+    public UserDAO User { get; set; }
+    public int Steps { get; set; }
+    public float SleepDuration { get; set; }
+    public int HeartRate { get; set; }
+}
