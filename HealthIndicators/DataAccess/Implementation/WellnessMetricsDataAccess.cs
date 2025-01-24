6bc82b78e245e30cf580324ef7c43957c39593ba
randrianalisoa.lalabritta <lala-britta-lau.randrianalisoa@etu.univ-littoral.fr>
2025-01-24 14:09:51 +0100

    7 last days + TU

--- HealthIndicators/DataAccess/Implementation/WellnessMetricsDataAccess.cs
+++ HealthIndicators/DataAccess/Implementation/WellnessMetricsDataAccess.cs
@@ -1,5 +1,6 @@
 using Common.DTO;
 using Common.Request;
+using Common.Response;
 using DataAccess.Interface;
 using Microsoft.EntityFrameworkCore;
 
@@ -31,4 +32,29 @@ public class WellnessMetricsDataAcess : IWellnessMetricsDataAccess
         await _context.SaveChangesAsync();
         return await GetWellnessMetricsById(newData.Entity.Id) ?? throw new NullReferenceException("Erreur lors de la creation des données de santé");
     }
+    
+    public async Task<UserLast7StepsResponse> GetUserLast7DaysSteps(int userId) {
+        var last7DaysData = await _context.WellnessMetrics
+            .Where(metric => metric.UserId == userId && metric.Date >= DateTime.UtcNow.AddDays(-7))
+            .ToListAsync();
+        
+        var maxStepsPerDay = last7DaysData
+            .GroupBy(metric => metric.Date.Date) 
+            .Select(group => new {
+                Date = group.Key.ToString("yyyy-MM-dd"),
+                MaxSteps = group.Max(metric => metric.Steps) 
+            }).ToList();
+
+        var response = new UserLast7StepsResponse {
+            Steps = maxStepsPerDay.Select(entry => new Dictionary<string, object> {
+                { "date", entry.Date },
+                { "steps", entry.MaxSteps }
+            }).ToList(),
+            TotalSteps = maxStepsPerDay.Sum(entry => entry.MaxSteps) 
+        };
+
+        return response;
+    }
+
+
 }
--- HealthIndicators/DataAccess/Interface/IWellnessMetricsDataAccess.cs
+++ HealthIndicators/DataAccess/Interface/IWellnessMetricsDataAccess.cs
@@ -1,9 +1,12 @@
 using Common.DTO;
 using Common.Request;
+using Common.Response;
 
 namespace DataAccess.Interface;
 public interface IWellnessMetricsDataAccess
 {
     Task<WellnessMetricsDAO?> GetWellnessMetricsById(int id);
+    
     Task<WellnessMetricsDAO> Create(WellnessMetricsCreationRequest request);
+    Task<UserLast7StepsResponse> GetUserLast7DaysSteps(int userId);
 }
